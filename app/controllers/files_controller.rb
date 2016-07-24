793afcd186c2c432a1931af94ded850c820db8c9
Mitch Joa <mitchjoa@gmail.com>
2016-07-24 09:43:47 -0500

    break out download into private method

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,14 +3,20 @@ class FilesController < ApplicationController
 
   def download
     begin
-      file = BookFile.find_by("book = ?", params[:id])
-      path = File.join(Rails.root, 'public', 'calibre-library', "#{file.book.path}", "#{file.name}.#{file.format}")
-
-      flash.notice = 'Book downloaded successfully.'
-      send_file path
+      download_book_with_id(params[:id])
     rescue
       flash.alert = 'Book could not be downloaded.'
       redirect_to root_path
     end
+  end
+
+  private
+
+  def download_book_with_id(book_id)
+    file = BookFile.find_by("book = ?", book_id)
+    path = File.join(Rails.root, 'public', 'calibre-library', "#{file.book.path}", "#{file.name}.#{file.format}")
+
+    flash.notice = 'Book downloaded successfully.'
+    send_file path
   end
 end
