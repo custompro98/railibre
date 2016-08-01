983b4df60f91d3464077d8053f5b124bd0373e93
Mitch Joa <mitchjoa@gmail.com>
2016-07-31 23:38:07 -0500

    Add books to calibre library

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A front end for Calibre written in Rails
 ## Install Instructions
 1. Create a symlink from `db/metadata.sqlite3` to `metadata.db` in your Calibre library folder
 2. Create a symlink from `public/calibre-library` to your Calibre library folder
+3. Ensure the calibre command line tools are in your path. (located inside calibre.app on OS X)
--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,3 +1,5 @@
+require 'os'
+
 class FilesController < ApplicationController
   def index
     # Displays the form
@@ -6,6 +8,7 @@ class FilesController < ApplicationController
   def download
     begin
       download_book_with_id(params[:id])
+      flash.notice = 'Book downloaded successfully.'
     rescue
       flash.alert = 'Book could not be downloaded.'
       redirect_to root_path
@@ -13,13 +16,15 @@ class FilesController < ApplicationController
   end
 
   def upload
-    # Does the heavylifting for upload
     uploaded_io = params[:ebook]
-    file_path = write_file(uploaded_io)
+    book_path = write_file(uploaded_io)
 
-    add_file_to_calibre_library(file_path)
+    if add_book_to_calibre_library(book_path)
+      flash.notice = "Book added to the Calibre library successfully"
+    else
+      flash.alert = "Book not added to the Calibre library."
+    end
 
-    flash.notice = "Book uploaded successfully."
     redirect_to root_path
   end
 
@@ -27,9 +32,8 @@ class FilesController < ApplicationController
 
   def download_book_with_id(book_id)
     file = BookFile.find_by("book = ?", book_id)
-    path = Rails.root.join('public', 'calibre-library', "#{file.book.path}", "#{file.name}.#{file.format}")
+    path = File.join(calibre_path, book_path(file))
 
-    flash.notice = 'Book downloaded successfully.'
     send_file path
   end
 
@@ -37,16 +41,36 @@ class FilesController < ApplicationController
     file_content = uploaded_io.tempfile.read
     file_content.force_encoding('UTF-8')
 
-    file_path = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
+    book_path = File.join(upload_path, uploaded_io.original_filename)
 
-    File.open(file_path, 'w') do |file|
+    File.open(book_path, 'w') do |file|
       file.write(file_content)
     end
 
-    file_path
+    book_path
   end
 
-  def add_file_to_calibre_library(ebook_file)
-    # Performs the system calls to add the book to calibre
+  def add_book_to_calibre_library(book_path)
+    if OS.mac?
+      system("/Applications/calibre.app/Contents/MacOS/calibredb add #{Shellwords.escape(book_path)} --library-path #{calibre_path}")
+    elsif OS.linux?
+      system("calibredb add #{Shellwords.escape(book_path)} --library-path #{calibre_path}")
+    else
+      false
+    end
+  end
+
+  private
+
+  def book_path(ebook_file)
+    File.join("#{ebook_file.book.path}", "#{ebook_file.name}.#{ebook_file.format}")
+  end
+
+  def calibre_path
+    Rails.root.join('public', 'calibre-library')
+  end
+
+  def upload_path
+    Rails.root.join('public', 'uploads')
   end
 end
